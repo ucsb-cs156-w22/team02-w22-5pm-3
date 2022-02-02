90bc3e3298230448ed58685c8f2bc92d1444182c
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-02-02 13:22:31 -0800

    mw - optimize imports and reformat

--- src/main/java/edu/ucsb/cs156/team02/repositories/UCSBSubjectRepository.java
+++ src/main/java/edu/ucsb/cs156/team02/repositories/UCSBSubjectRepository.java
@@ -5,9 +5,7 @@ import edu.ucsb.cs156.team02.entities.UCSBSubject;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UCSBSubjectRepository extends CrudRepository<UCSBSubject, Long> {
-  Iterable<UCSBSubject> findAllBySubjectCode(String subject);
+    Iterable<UCSBSubject> findAllBySubjectCode(String subject);
 }

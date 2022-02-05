ed6c45ba50f88834603c5aadb1cd3fb6aed13cfe
Tyler Ekaireb <tylerekaireb@gmail.com>
2022-02-04 19:57:15 -0800

    te - removed unused imports

--- src/main/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementController.java
+++ src/main/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementController.java
@@ -1,35 +1,27 @@
 package edu.ucsb.cs156.team02.controllers;
 
 import edu.ucsb.cs156.team02.entities.UCSBRequirement;
-import edu.ucsb.cs156.team02.models.CurrentUser;
 import edu.ucsb.cs156.team02.repositories.UCSBRequirementRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import lombok.extern.slf4j.Slf4j;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.Optional;
 
 @Api(description = "UCSB Requirements")
 @RequestMapping("/api/UCSBRequirements")
 @RestController
-@Slf4j
 public class UCSBRequirementController extends ApiController {
 
     public class UCSBRequirementOrError {

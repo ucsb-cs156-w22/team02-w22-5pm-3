c41a1b6a97c0c0b0a35a3a98be10ab39008d6830
qianyouwang <56094380+qianyouwang@users.noreply.github.com>
2022-02-07 12:38:09 -0800

    Update src/main/java/edu/ucsb/cs156/team02/repositories/CollegiateSubredditRepository.java
    Co-authored-by: SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>

--- src/main/java/edu/ucsb/cs156/team02/repositories/CollegiateSubredditRepository.java
+++ src/main/java/edu/ucsb/cs156/team02/repositories/CollegiateSubredditRepository.java
@@ -4,8 +4,6 @@ import edu.ucsb.cs156.team02.entities.CollegiateSubreddit;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CollegiateSubredditRepository extends CrudRepository<CollegiateSubreddit, Long> {
   Iterable<CollegiateSubreddit> findByName(String name);

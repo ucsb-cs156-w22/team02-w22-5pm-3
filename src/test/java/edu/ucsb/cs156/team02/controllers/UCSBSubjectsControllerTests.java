fe8021020f7a9346df6d9b06607dc5fabd784df0
nicg6645 <41531696+nicg6645@users.noreply.github.com>
2022-02-07 13:15:28 -0800

    ng #18 tests compile now

--- src/test/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/team02/controllers/UCSBSubjectsControllerTests.java
@@ -29,7 +29,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@WebMvcTest(controllers = UCSBSubjectsController.class)
+@WebMvcTest(controllers = UCSBSubjectController.class)
 @Import(TestConfig.class)
 public class UCSBSubjectsControllerTests extends ControllerTestCase {
 

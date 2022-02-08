2b8705a16f42db3239bab0196b2b9e1d66a38431
Tyler Ekaireb <tekaireb@users.noreply.github.com>
2022-02-07 17:44:39 -0800

    UCSBRequirements GET (#36)
    * te - added UCSBRequirement entity and repository
    
    * te - started working on UCSBRequirementController
    
    * Implemented /UCSBRequirements?id=X api endpoint and associated tests
    
    * te - removed unused imports
    
    * Update src/test/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementControllerTests.java
    
    Co-authored-by: SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
    
    * Update src/test/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementControllerTests.java
    
    Co-authored-by: SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
    
    * Update src/test/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementControllerTests.java
    
    Co-authored-by: SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
    
    * Update src/test/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementControllerTests.java
    
    Co-authored-by: SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
    
    * Update src/test/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementControllerTests.java
    
    Co-authored-by: SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
    
    * Update src/test/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementControllerTests.java
    
    Co-authored-by: SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
    
    Co-authored-by: SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>

---
+++ src/main/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementController.java
@@ -0,0 +1,76 @@
+package edu.ucsb.cs156.team02.controllers;
+
+import edu.ucsb.cs156.team02.entities.UCSBRequirement;
+import edu.ucsb.cs156.team02.repositories.UCSBRequirementRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@Api(description = "UCSB Requirements")
+@RequestMapping("/api/UCSBRequirements")
+@RestController
+public class UCSBRequirementController extends ApiController {
+
+    public class UCSBRequirementOrError {
+        Long id;
+        UCSBRequirement req;
+        ResponseEntity<String> error;
+
+        public UCSBRequirementOrError(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Autowired
+    UCSBRequirementRepository ucsbRequirementRepository;
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @ApiOperation(value = "Get a single requirement by ID")
+    // @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public ResponseEntity<String> getUCSBRequirementById(
+            @ApiParam("id") @RequestParam Long id) throws JsonProcessingException {
+        loggingService.logMethod();
+        UCSBRequirementOrError roe = new UCSBRequirementOrError(id);
+
+        roe = doesUCSBRequirementExist(roe);
+        if (roe.error != null) {
+            return roe.error;
+        }
+        
+        String body = mapper.writeValueAsString(roe.req);
+        return ResponseEntity.ok().body(body);
+    }
+
+    
+
+    public UCSBRequirementOrError doesUCSBRequirementExist(UCSBRequirementOrError roe) {
+
+        Optional<UCSBRequirement> optionalReq = ucsbRequirementRepository.findById(roe.id);
+
+        if (optionalReq.isEmpty()) {
+            roe.error = ResponseEntity
+                    .badRequest()
+                    .body(String.format("requirement with id %d not found", roe.id));
+        } else {
+            roe.req = optionalReq.get();
+        }
+        return roe;
+    }
+
+}
---
+++ src/test/java/edu/ucsb/cs156/team02/controllers/UCSBRequirementControllerTests.java
@@ -0,0 +1,139 @@
+package edu.ucsb.cs156.team02.controllers;
+
+import edu.ucsb.cs156.team02.repositories.UCSBRequirementRepository;
+import edu.ucsb.cs156.team02.testconfig.TestConfig;
+import edu.ucsb.cs156.team02.ControllerTestCase;
+import edu.ucsb.cs156.team02.entities.UCSBRequirement;
+import edu.ucsb.cs156.team02.repositories.UserRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = UCSBRequirementController.class)
+@Import(TestConfig.class)
+public class UCSBRequirementControllerTests extends ControllerTestCase {
+
+    @MockBean
+    UCSBRequirementRepository ucsbRequirementRepository;
+
+    @MockBean
+    UserRepository userRepository;
+
+    // Tests with mocks for database actions
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void api_reqs__user_logged_in__returns_a_req_that_exists() throws Exception {
+
+        // arrange
+
+        UCSBRequirement req1 = UCSBRequirement.builder()
+                .requirementCode("X")
+                .requirementTranslation("X")
+                .collegeCode("X")
+                .objCode("X")
+                .courseCount(0)
+                .units(0)
+                .inactive(false)
+                .id(7L).build();
+
+        when(ucsbRequirementRepository.findById(eq(7L))).thenReturn(Optional.of(req1));
+
+        // act
+
+        MvcResult response = mockMvc.perform(get("/api/UCSBRequirements?id=7"))
+                .andExpect(status().isOk()).andReturn();
+
+        // assert
+
+        verify(ucsbRequirementRepository, times(1)).findById(eq(7L));
+        String expectedJson = mapper.writeValueAsString(req1);
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals(expectedJson, responseString);
+    }
+
+    @WithMockUser(roles = { "USER" })
+    @Test
+    public void api_reqs__user_logged_in__search_for_req_that_does_not_exist() throws Exception {
+
+        // arrange
+
+        when(ucsbRequirementRepository.findById(eq(7L))).thenReturn(Optional.empty());
+
+        // act
+        MvcResult response = mockMvc.perform(get("/api/UCSBRequirements?id=7"))
+                .andExpect(status().isBadRequest()).andReturn();
+
+        // assert
+
+        verify(ucsbRequirementRepository, times(1)).findById(eq(7L));
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals("requirement with id 7 not found", responseString);
+    }
+
+    @WithMockUser(roles = { "ADMIN" })
+    @Test
+    public void api_reqs__admin_logged_in__returns_a_req_that_exists() throws Exception {
+
+        // arrange
+
+        UCSBRequirement req1 = UCSBRequirement.builder()
+                .requirementCode("X")
+                .requirementTranslation("X")
+                .collegeCode("X")
+                .objCode("X")
+                .courseCount(0)
+                .units(0)
+                .inactive(false)
+                .id(7L).build();
+
+        when(ucsbRequirementRepository.findById(eq(7L))).thenReturn(Optional.of(req1));
+
+        // act
+
+        MvcResult response = mockMvc.perform(get("/api/UCSBRequirements?id=7"))
+                .andExpect(status().isOk()).andReturn();
+
+        // assert
+
+        verify(ucsbRequirementRepository, times(1)).findById(eq(7L));
+        String expectedJson = mapper.writeValueAsString(req1);
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals(expectedJson, responseString);
+    }
+
+    @WithMockUser(roles = { "ADMIN" })
+    @Test
+    public void api_req__admin_logged_in__search_for_req_that_does_not_exist() throws Exception {
+
+        // arrange
+
+        when(ucsbRequirementRepository.findById(eq(29L))).thenReturn(Optional.empty());
+
+        // act
+        MvcResult response = mockMvc.perform(get("/api/UCSBRequirements?id=29"))
+                .andExpect(status().isBadRequest()).andReturn();
+
+        // assert
+
+        verify(ucsbRequirementRepository, times(1)).findById(eq(29L));
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals("requirement with id 29 not found", responseString);
+    }
+
+}

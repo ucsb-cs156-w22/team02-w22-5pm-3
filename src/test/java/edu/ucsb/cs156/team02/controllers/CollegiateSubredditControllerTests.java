eae12194e4001b14e2dc49e846357e5119ac1d54
qianyouwang <qianyou_wang@umail.ucsb.edu>
2022-02-08 18:15:16 -0800

    perfected test cases

--- src/test/java/edu/ucsb/cs156/team02/controllers/CollegiateSubredditControllerTests.java
+++ src/test/java/edu/ucsb/cs156/team02/controllers/CollegiateSubredditControllerTests.java
@@ -302,11 +302,30 @@ public class CollegiateSubredditControllerTests extends ControllerTestCase {
     }
 
 
-    
-
-//     @WithMockUser(roles = { "USER" })
     @Test
-    public void api_collegiateSubreddit__user_logged_in__delete_subreddit_that_does_not_exist() throws Exception {
+    public void api_CollegiateSubreddit__delete_exist() throws Exception {
+
+        // arrange
+        CollegiateSubreddit collegiateSubreddit1 = CollegiateSubreddit.builder().name("CollegiateSubreddit1").location("CollegiateSubreddit1").subreddit("CollegiateSubreddit1").id(123L).build();
+        when(collegiateSubredditRepository.findById(eq(123L))).thenReturn(Optional.of(collegiateSubreddit1));
+
+
+        // act
+        MvcResult response = mockMvc.perform(
+                delete("/api/collegiateSubreddits?id=123")
+                        .with(csrf()))
+                .andExpect(status().isOk()).andReturn();
+
+        // assert
+        verify(collegiateSubredditRepository, times(1)).findById(123L);
+        verify(collegiateSubredditRepository, times(1)).deleteById(123L);
+        String responseString = response.getResponse().getContentAsString();
+        assertEquals("subreddit with id 123 deleted", responseString);
+    }
+
+
+    @Test
+    public void api_collegiateSubreddit__delete_does_not_exist() throws Exception {
         // arrange
 
         User otherUser = User.builder().id(98L).build();
